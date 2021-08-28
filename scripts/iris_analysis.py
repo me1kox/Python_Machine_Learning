42df499c46106944bde9e05487eae57d4dd2047b
mekox <k.j.obuya1@sheffield.ac.uk>
2021-08-28 11:57:44 +0100

    Added comments

--- scripts/iris_analysis.py
+++ scripts/iris_analysis.py
@@ -1,22 +1,25 @@
 from sklearn import datasets
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import  StandardScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron
 
+# loading in the iris dataset
 iris = datasets.load_iris()
-X = iris.data[:,[2,3]]
-y = iris.target
+X = iris.data[:, [2, 3]]  # assign instances to X; only interested in third and fourth column
+y = iris.target  # assign outputs to y
 
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3, random_state=1, stratify=y)
+# split the dataset into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
 
+# perform preprocessing by standardization
 sc = StandardScaler()
-sc.fit(X_train)
-X_train_std = sc.transform(X_train)
-X_test_std = sc.transform(X_test)
+sc.fit(X_train) # fit mean and variation of standard scaler object to training dataset
+X_train_std = sc.transform(X_train) # standardize training data set
+X_test_std = sc.transform(X_test) # standardize test data set
 
-ppn = Perceptron(eta0=0.1,random_state=1)
-ppn.fit(X_train_std,y_train)
+ppn = Perceptron(eta0=0.1, random_state=1) # instantiate perceptron object; set hyperparameters
+ppn.fit(X_train_std, y_train) # update weights using training dataset
 
-y_pred = ppn.predict(X_test_std)
-print('Accuracy: %.3f' % ppn.score(X_test_std,y_test))
+y_pred = ppn.predict(X_test_std) # run trained perceptron on test data set and collect outputs
+print('Accuracy: %.3f' % ppn.score(X_test_std, y_test)) # display accuracy of predictions on test data set
